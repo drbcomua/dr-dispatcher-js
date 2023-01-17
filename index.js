300412a8e371cb55da294ed6cce4683c2082894d
Dmitry RUMYANTSEV <dr@b.com.ua>
2023-01-17 11:30:47 -0600

    Add existing dispatcher demo on JavaScript

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+let storage = {}
+
+function stringToHash(string) {
+  var hash = 0;
+
+  if (string.length == 0) return hash;
+
+  for (i = 0; i < string.length; i++) {
+    char = string.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash;
+  }
+
+  return hash;
+}
+
+function nameToHash(name) {
+  return stringToHash(name.firstName) ^ stringToHash(name.lastName)
+}
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.post('/name', (request, response) => {
+  const names = request.body
+  let resp = {}
+  let temp = {}
+  names.forEach(name => {
+    if(nameToHash(name) in storage) {
+      resp[nameToHash(name)] = name
+    } else {
+      temp[nameToHash(name)] = name
+    }
+  })
+  Object.keys(temp).forEach(key => storage[key] = temp[key])
+  response.status(200).send(Object.values(resp))
+})
+
+app.delete('/name', (request, response) => {
+  request.body.forEach(name => delete storage[nameToHash(name)])
+  response.status(200).send("")
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
